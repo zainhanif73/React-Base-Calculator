f4c0ffff9fac60fe8f00b5ae8bf2fb514a51ae93
ZAINHANIF <www.zainhanif73@gmail.com>
2022-08-29 11:55:28 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,97 +1,128 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import Item from "./Item";
+import "./index.css";
+// import { faL } from "@fortawesome/free-solid-svg-icons";
 
-let sum = 0;
-let pre = 0;
-let sumCheck = false;
-let subCheck = false;
-let mulCheck = false;
-let divCheck = false;
-
+var previousValue = "";
 export default function App() {
-  const [value, setValue] = useState(0);
-  const [store, setstore] = useState(0);
+  const [items, setItems] = useState([]);
+  const [itemsEditable, setItemsEditable] = useState([]);
+  const inputItem = useRef("");
 
-  function getValue(digit) {
-    setValue(value + digit);
-
-    setstore(store + digit);
-
-    console.log(store + digit);
-
-    if (digit === "reset") {
-      setValue(0);
-      setstore(0);
-      pre = 0;
-    }
-
-    if (sumCheck) {
-      sumCheck = false;
-      setValue(sum + parseInt(digit));
-    } else if (subCheck) {
-      setValue(sum - parseInt(digit));
-      subCheck = false;
-    } else if (mulCheck) {
-      setValue(sum * parseInt(digit));
-      mulCheck = false;
-    } else if (divCheck) {
-      setValue(sum / parseInt(digit));
-      divCheck = false;
-    }
-
-    if (digit === "+") {
-      sumCheck = true;
-    } else if (digit === "-") {
-      subCheck = true;
-    } else if (digit === "*") {
-      mulCheck = true;
-    } else if (digit === "/") {
-      divCheck = true;
-    }
-
-    if (pre === 0) {
-      sum = parseInt(digit);
-      pre = 1;
-    } else {
-      sum = value;
+  function addItem() {
+    let c = inputItem.current.value;
+    if (c !== "") {
+      setItems([...items, c]);
+      setItemsEditable([...itemsEditable, false]);
+      inputItem.current.value = "";
     }
   }
+
+  function deleteItem(index, check) {
+    if (!check) setItems(items.filter((value, number) => index !== number));
+    else {
+      editable(index, check);
+      items[index] = previousValue;
+      setItems(items);
+    }
+  }
+
+  function editable(index, check) {
+    if (!check) {
+      const temp = [...itemsEditable];
+      temp[index] = true;
+      setItemsEditable(temp);
+      previousValue = items[index];
+    } else {
+      const temp = [...itemsEditable];
+      temp[index] = false;
+      setItemsEditable(temp);
+    }
+    console.log(previousValue);
+  }
+
   return (
-    <div className="App">
-      <div className="calculator">
-        <div className="display">
-          <span
-            className="answer"
-            style={{ marginLeft: "15px", color: "white" }}
-          >
-            {value}
-          </span>
-        </div>
+    <>
+      <div className="main">
+        <div className="main-body">
+          <div className="temp">
+            <div className="heading"> ToDo List</div>
+            <div className="add-data">
+              <input
+                className="input"
+                ref={inputItem}
+                type="text"
+                name="item"
+                id="item"
+                placeholder="Add an Item"
+              ></input>
+              <button className="add-btn" onClick={(item) => addItem(item)}>
+                +
+              </button>
+            </div>
+            <div className="add-item">
+              {items.map((e, index) => {
+                return (
+                  <div className="items" key={index}>
+                    <input
+                      disabled={!itemsEditable[index]}
+                      className={
+                        itemsEditable[index] ? "editText" : "inputItem"
+                      }
+                      type="text"
+                      value={items[index]}
+                      onChange={(e1) => {
+                        const temp = [...items];
+                        temp[index] = e1.target.value;
+                        setItems(temp);
+                      }}
+                    />
+                    <button
+                      className="delete-btn"
+                      onClick={() => deleteItem(index, itemsEditable[index])}
+                    >
+                      <span
+                        style={{
+                          display: !itemsEditable[index] ? "" : "none",
+                        }}
+                      >
+                        Delete
+                      </span>
 
-        <hr />
-
-        <div className="digits">
-          <button onClick={() => getValue(1)}>1</button>
-          <button onClick={() => getValue(2)}>2</button>
-          <button onClick={() => getValue(3)}>3</button>
-          <button onClick={() => getValue("-")}>-</button>
-          <br />
-          <button onClick={() => getValue(4)}>4</button>
-          <button onClick={() => getValue(5)}>5</button>
-          <button onClick={() => getValue(6)}>6</button>
-          <button onClick={() => getValue("/")}>/</button>
-          <br />
-          <button onClick={() => getValue(7)}>7</button>
-          <button onClick={() => getValue(8)}>8</button>
-          <button onClick={() => getValue(9)}>9</button>
-          <button onClick={() => getValue("*")}>*</button>
-          <br />
-          {/* <button></button> */}
-          <button onClick={() => getValue(0)}>0</button>
-          <button onClick={() => getValue("reset")}>R</button>
-          <button onClick={() => getValue("=")}>=</button>
-          <button onClick={() => getValue("+")}>+</button>
+                      <span
+                        style={{
+                          display: itemsEditable[index] ? "" : "none",
+                        }}
+                      >
+                        Cancel
+                      </span>
+                    </button>
+                    <button
+                      className="edit-btn"
+                      onClick={() => editable(index, itemsEditable[index])}
+                    >
+                      <span
+                        style={{
+                          display: !itemsEditable[index] ? "" : "none",
+                        }}
+                      >
+                        Edit
+                      </span>
+                      <span
+                        style={{
+                          display: itemsEditable[index] ? "" : "none",
+                        }}
+                      >
+                        Ok
+                      </span>
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
